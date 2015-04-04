820f0c36cc23bab8db01e97a5247e93cd3a3525e
adhavle <adhavle@marklogic.com>
2015-04-03 23:54:13 -0400

    removing typeahead from ui

--- dist/all.js
+++ dist/all.js
@@ -635,20 +635,7 @@ angular.module('sample.common', [])
         ctrl.removeSearchTag = function(index) {
           consumerSearchService.removeSearchTag(index);
         }
-      $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
-      ctrl.getTypeaheadValues = function($viewValue) {
-        var someOutput = "";//return something
-        alert("hi");
-        $http({
-          method: 'GET',
-          url: 'api/v1/person?name__icontains=' + $viewValue
-        }).error(function (data) {
-          console.error(data);
-        }).success(function (data) {
-          console.log(data);//Do whatever you want
-        });
-        return someOutput;
-      }
+
         $scope.$watch(function() { return consumerSearchService.isSearching; }, function(newVal,oldVal) {
           ctrl.isSearching = newVal;
         });
